1347e6773f797a3daecad7594e2e25149fbe50a4
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-02-05 05:23:11 +0800

    Update final2021(1)_2.c

--- Final2021(1)/final2021(1)_2.c
+++ Final2021(1)/final2021(1)_2.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 //Function Declaration (Prototype)
-void sort_bubble(int num[], int totalNum);
+void sort_bubbleDesc(int num[], int totalNum, int max);
+void sort_bubbleAsc(int num[], int totalNum, int max);
 
 int main(void){
 
-    int totalNum, loop;
+    int totalNum, max, loop;
+    int num1[5]={7, 5, 3, 8, 1}, num2[5]={7, 5, 3, 1, 8};
 
     printf("Enter list size: ");
     scanf(" %d", &totalNum);
 
+    max=totalNum;
     int num[totalNum];
 
     printf("\n");
@@ -18,24 +21,31 @@ int main(void){
         printf("Enter number: ");
         scanf(" %d", &num[loop]);}
 
-    printf("\nBefore sorting    :");
-    for(loop=0;loop<totalNum;loop++){
-        printf(" %d", num[loop]);}
+    //Sorting process
+    printf("\nSorting process:-\n\n");
 
-    printf("\n\nSorting process:-\n\n");
-    sort_bubble(num, totalNum);
-
-    printf("\nSorted list       :");
     for(loop=0;loop<totalNum;loop++){
-        printf(" %d", num[loop]);}
+        printf(" %d", num1[loop]);}
+    printf("\n");
+    sort_bubbleDesc(num1, totalNum, max);
+
+    for(loop=0;loop<totalNum;loop++){
+        printf(" %d", num2[loop]);}
+    printf("\n");
+    sort_bubbleAsc(num2, totalNum, max);
+
+    //Sorted list
+    printf(" Sorted list:");
+    for(loop=0;loop<totalNum;loop++){
+        printf(" %d", num2[loop]);}
 
     printf("\n");
 
 return 0;
 }
 
-//Function Definition: sort_bubble
-void sort_bubble(int num[], int totalNum){
+//Function Definition: sort_bubbleDesc
+void sort_bubbleDesc(int num[], int totalNum, int max){
 
     int temp, loop;
 
@@ -43,15 +53,42 @@ void sort_bubble(int num[], int totalNum){
         totalNum--;
 
         for(loop=0;loop<totalNum;loop++){
+            if(num[loop]<num[loop+1]){
+                temp=num[loop];
+                num[loop]=num[loop+1];
+                num[loop+1]=temp;}
+        }
 
+        for(loop=0;loop<max;loop++){
+            if(totalNum>1){
+                printf(" %d", num[loop]);}
+        }
+
+        printf("\n");
+
+    }
+
+}
+
+//Function Definition: sort_bubbleAsc
+void sort_bubbleAsc(int num[], int totalNum, int max){
+
+    int temp, loop;
+
+    while(totalNum>1){
+        totalNum--;
+
+        for(loop=0;loop<totalNum;loop++){
             if(num[loop]>num[loop+1]){
                 temp=num[loop];
                 num[loop]=num[loop+1];
                 num[loop+1]=temp;}
         }
 
-        for(loop=0;loop<5;loop++){
-            printf(" %d", num[loop]);}
+        for(loop=0;loop<max;loop++){
+            if(totalNum>1){
+                printf(" %d", num[loop]);}
+        }
 
         printf("\n");
 
