f86b67687094010985ccc6f0d5ac2b4d4c572054
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-02-12 20:38:33 +0800

    Create final2022(1)_1.c

---
+++ Final2022(1)/final2022(1)_1.c
@@ -0,0 +1,198 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Structure Declaration (Template)
+struct River{
+
+    //Data Field
+    char state[20];
+    char riverName[30];
+    int waterQualityIndex;
+    char category[5];
+
+    //Pointer (Link) Field
+    struct River *ptrNext;};
+
+struct River *ptrHead, *ptrNew, *ptrCurrent;
+
+//Function Declaration (Prototype)
+void insert_riverInfo();
+void display_riverInfo();
+
+int main(void){
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Kelantan");
+    strcpy(ptrNew->riverName, "Sg. Alor B");
+    ptrNew->waterQualityIndex=50;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Pahang");
+    strcpy(ptrNew->riverName, "Sg. Jebong");
+    ptrNew->waterQualityIndex=56;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Pahang");
+    strcpy(ptrNew->riverName, "Sg. Singol");
+    ptrNew->waterQualityIndex=57;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Pahang");
+    strcpy(ptrNew->riverName, "Sg. Semberong");
+    ptrNew->waterQualityIndex=59;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Terengganu");
+    strcpy(ptrNew->riverName, "Sg. Ransan");
+    ptrNew->waterQualityIndex=68;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Terengganu");
+    strcpy(ptrNew->riverName, "Sg. Ruang");
+    ptrNew->waterQualityIndex=71;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Kelantan");
+    strcpy(ptrNew->riverName, "Sg. Keladi");
+    ptrNew->waterQualityIndex=74;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Pahang");
+    strcpy(ptrNew->riverName, "Sg. Bebar");
+    ptrNew->waterQualityIndex=75;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Kelantan");
+    strcpy(ptrNew->riverName, "Sg. Pengkalan Chepa");
+    ptrNew->waterQualityIndex=76;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Pahang");
+    strcpy(ptrNew->riverName, "Sg. Belat");
+    ptrNew->waterQualityIndex=77;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Pahang");
+    strcpy(ptrNew->riverName, "Sg. Tekam");
+    ptrNew->waterQualityIndex=78;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Kelantan");
+    strcpy(ptrNew->riverName, "Sg. Kelantan");
+    ptrNew->waterQualityIndex=79;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Pahang");
+    strcpy(ptrNew->riverName, "Sg. Jengka");
+    ptrNew->waterQualityIndex=80;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Pahang");
+    strcpy(ptrNew->riverName, "Sg. Pandan");
+    ptrNew->waterQualityIndex=82;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Kelantan");
+    strcpy(ptrNew->riverName, "Sg. Berok");
+    ptrNew->waterQualityIndex=84;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Terengganu");
+    strcpy(ptrNew->riverName, "Sg. Dungun");
+    ptrNew->waterQualityIndex=85;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Terengganu");
+    strcpy(ptrNew->riverName, "Sg. Besut");
+    ptrNew->waterQualityIndex=86;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Kelantan");
+    strcpy(ptrNew->riverName, "Sg. Betis");
+    ptrNew->waterQualityIndex=87;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Pahang");
+    strcpy(ptrNew->riverName, "Sg. Burung");
+    ptrNew->waterQualityIndex=90;
+    insert_riverInfo();
+
+    ptrNew=(struct River *)malloc(sizeof(struct River));
+    strcpy(ptrNew->state, "Kelantan");
+    strcpy(ptrNew->riverName, "Sg. Ber");
+    ptrNew->waterQualityIndex=91;
+    insert_riverInfo();
+
+    display_riverInfo();
+
+return 0;
+}
+
+//Function Definition: insert_riverInfo
+void insert_riverInfo(){
+
+    if(ptrHead==NULL){
+        ptrHead=ptrNew;
+        ptrNew->ptrNext=NULL;}
+
+    else{
+        ptrCurrent=ptrHead;
+
+        while(ptrCurrent->ptrNext!=NULL){
+            ptrCurrent=ptrCurrent->ptrNext;}
+
+        ptrCurrent->ptrNext=ptrNew;
+        ptrNew->ptrNext=NULL;}
+
+}
+
+//Function Definition: display_riverInfo
+void display_riverInfo(){
+
+    if(ptrHead==NULL){
+        printf("No river registered in the system yet.\n");}
+
+    else{
+        printf("State\t\t River\t\t\t WQI\t Category\n");
+        printf("=========================================================\n");
+        ptrCurrent=ptrHead;
+
+        do{
+
+            if(ptrCurrent->waterQualityIndex<60){
+                strcpy(ptrCurrent->category, "P");}
+
+            else if(ptrCurrent->waterQualityIndex<=80){
+                strcpy(ptrCurrent->category, "SP");}
+
+            else{
+                strcpy(ptrCurrent->category, "C");}
+
+            printf("%-14s %-25s %-10d %s", ptrCurrent->state, ptrCurrent->riverName, ptrCurrent->waterQualityIndex, ptrCurrent->category);
+
+            printf("\n");
+
+            ptrCurrent=ptrCurrent->ptrNext;
+
+        }while(ptrCurrent!=NULL);}
+
+}
